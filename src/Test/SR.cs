36cd6ab69dabcf8b40bfd22cd83e7b309c0390be
Funny_Silkie <ban.grumble99rar@gmail.com>
2023-07-07 17:50:38 +0900

    implement sequence test

---
+++ src/Test/SR.cs
@@ -0,0 +1,19 @@
+﻿namespace Test
+{
+    /// <summary>
+    /// 静的リソースを管理します。
+    /// </summary>
+    internal static class SR
+    {
+        /// <summary>
+        /// テストデータの配置されるディレクトリ名
+        /// </summary>
+        internal const string SourceDir = "TestData";
+
+        #region TestData Files
+
+        internal const string File_SmallData = "SmallData.fasta";
+
+        #endregion TestData Files
+    }
+}
---
+++ src/Test/Util.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Test
+{
+    /// <summary>
+    /// 共通処理を記述します。
+    /// </summary>
+    internal static class Util
+    {
+        /// <summary>
+        /// テスト用データのファイルパスを取得します。
+        /// </summary>
+        /// <param name="filename">ファイル名</param>
+        /// <returns>テスト用データのファイルパス</returns>
+        public static string GetDataFilePath(string filename) => Path.Combine(SR.SourceDir, filename);
+    }
+}
